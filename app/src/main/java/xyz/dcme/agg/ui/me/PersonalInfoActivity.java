831b964dc784c80902610941f139ff3b780a3ac8
caoshen <ceeseemail@gmail.com>
2017-03-31 23:57:51 +0800

    add personal info

--- app/src/main/java/xyz/dcme/agg/ui/me/PersonalInfoActivity.java
+++ app/src/main/java/xyz/dcme/agg/ui/me/PersonalInfoActivity.java
@@ -1,20 +1,61 @@
 package xyz.dcme.agg.ui.me;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import xyz.dcme.agg.R;
 import xyz.dcme.agg.ui.BaseActivity;
+import xyz.dcme.agg.ui.login.AccountInfo;
+import xyz.dcme.agg.util.Constants;
 
 public class PersonalInfoActivity extends BaseActivity {
+
+    private Toolbar mToolbar;
+    private String mUserName;
+    private String mAvatar;
+    private AccountInfo mAccountInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
+        getIntentData();
+        initViews();
     }
 
-    public static void showPersonalInfo(FragmentActivity activity) {
+    private void getIntentData() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            mAccountInfo = intent.getParcelableExtra(Constants.EXTRA_ACCOUNT_INFO);
 
+        }
+        if (mAccountInfo != null) {
+            mUserName = mAccountInfo.getUserName();
+            mAvatar = mAccountInfo.getAvatarUrl();
+        }
     }
+
+    private void initViews() {
+        mToolbar = getToolbar();
+        mToolbar.setTitle(mUserName);
+        mToolbar.inflateMenu(R.menu.menu_personal_info);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.item_share) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
