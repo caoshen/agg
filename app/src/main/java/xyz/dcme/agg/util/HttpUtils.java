359ab2cf45ecf1ba8de3c85fa06c457a8303bd7f
caoshen <ceeseemail@gmail.com>
2017-06-14 13:29:47 +0800

    add OkHttpUtils

--- app/src/main/java/xyz/dcme/agg/util/HttpUtils.java
+++ app/src/main/java/xyz/dcme/agg/util/HttpUtils.java
@@ -1,22 +1,41 @@
 package xyz.dcme.agg.util;
 
-import android.content.Context;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.Map;
 
 public class HttpUtils {
     private static HttpUtils sInstance;
 
-    public HttpUtils(Context context) {
+    private HttpUtils() {
 
     }
 
-    public static HttpUtils getInstance(Context context) {
+    public static HttpUtils getInstance() {
         if (sInstance == null) {
             synchronized (HttpUtils.class) {
                 if (sInstance == null) {
-                    sInstance = new HttpUtils(context);
+                    sInstance = new HttpUtils();
                 }
             }
         }
         return sInstance;
+    }
+
+    public void get(String url, StringCallback callback) {
+        OkHttpUtils.get()
+                .url(url)
+                .build()
+                .execute(callback);
+    }
+
+    public void post(String url, Map<String, String> headers, Map<String, String> params, StringCallback callback) {
+        OkHttpUtils.post()
+                .url(url)
+                .headers(headers)
+                .params(params)
+                .build()
+                .execute(callback);
     }
 }
