ba8c72d60fc4c06ca4d683ccd91ad71b73edc6ea
caoshen <ceeseemail@gmail.com>
2017-06-22 22:59:48 +0800

    modify personal info collapsing style

--- app/src/main/java/xyz/dcme/agg/ui/personal/PersonalInfoActivity.java
+++ app/src/main/java/xyz/dcme/agg/ui/personal/PersonalInfoActivity.java
@@ -3,25 +3,29 @@ package xyz.dcme.agg.ui.personal;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.bumptech.glide.Glide;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import xyz.dcme.agg.R;
-import xyz.dcme.agg.ui.BaseActivity;
 import xyz.dcme.agg.account.AccountInfo;
+import xyz.dcme.agg.ui.BaseActivity;
 import xyz.dcme.agg.util.Constants;
+import xyz.dcme.agg.util.LogUtils;
 
 public class PersonalInfoActivity extends BaseActivity {
 
+    private static final String LOG_TAG = "PersonalInfoActivity";
     private Toolbar mToolbar;
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
     private String mUserName;
@@ -29,6 +33,14 @@ public class PersonalInfoActivity extends BaseActivity {
     private AccountInfo mAccountInfo;
     private CircleImageView mImage;
     private ViewPager mPager;
+    private TabLayout mTab;
+    private AppBarLayout mAppBarLayout;
+
+    public static void start(Context context, AccountInfo info) {
+        Intent intent = new Intent(context, PersonalInfoActivity.class);
+        intent.putExtra(Constants.EXTRA_ACCOUNT_INFO, info);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,12 +72,20 @@ public class PersonalInfoActivity extends BaseActivity {
     }
 
     private void initViews() {
+
         mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing);
 
-        mToolbar = getToolbar();
-        ActionBar ab = getSupportActionBar();
-        if (!TextUtils.isEmpty(mUserName) && ab != null) {
-            ab.setTitle(mUserName);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        if (!TextUtils.isEmpty(mUserName)) {
+            mToolbar.setTitle(mUserName);
         }
 
         mImage = (CircleImageView) findViewById(R.id.avatar);
@@ -77,6 +97,22 @@ public class PersonalInfoActivity extends BaseActivity {
         FragmentManager fm = getSupportFragmentManager();
         PersonalPagerAdapter adapter = new PersonalPagerAdapter(this, fm, mUserName);
         mPager.setAdapter(adapter);
+        mTab = (TabLayout) findViewById(R.id.info_tabs);
+        mTab.setupWithViewPager(mPager);
+
+        mAppBarLayout = (AppBarLayout) findViewById(R.id.appbar_info);
+        mAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            int scrollRange = -1;
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                float ratio = 1 - Math.abs(verticalOffset * 1.0F / (scrollRange));
+                mImage.setAlpha(ratio);
+                LogUtils.d(LOG_TAG, "initViews -> onOffsetChanged -> ratio: " + ratio);
+            }
+        });
     }
 
     @Override
@@ -90,11 +126,5 @@ public class PersonalInfoActivity extends BaseActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public static void start(Context context, AccountInfo info) {
-        Intent intent = new Intent(context, PersonalInfoActivity.class);
-        intent.putExtra(Constants.EXTRA_ACCOUNT_INFO, info);
-        context.startActivity(intent);
     }
 }
