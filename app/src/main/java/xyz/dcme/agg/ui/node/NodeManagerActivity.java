0148d7ab713741cc3d0fdfe5a463c800cd646c2a
caoshen <ceeseemail@gmail.com>
2017-09-08 23:03:36 +0800

    delete useless code

--- app/src/main/java/xyz/dcme/agg/ui/node/NodeManagerActivity.java
+++ app/src/main/java/xyz/dcme/agg/ui/node/NodeManagerActivity.java
@@ -3,27 +3,15 @@ package xyz.dcme.agg.ui.node;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
-
-import com.google.android.flexbox.FlexboxLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import xyz.dcme.agg.R;
-import xyz.dcme.agg.widget.flowlayout.TagView;
 import xyz.dcme.library.base.BaseActivity;
 
 
 public class NodeManagerActivity extends BaseActivity {
-
-    private LayoutInflater mInflater;
-    private String[] life = {"楼市房产", "城市建设", "我爱我家"};
-    private String[] social = {"购物", "找工作", "非诚勿扰", "二手交易", "教育", "公益"};
-    private String[] tech = {"IT技术", "汽车", "硬件数码", "金融财经", "生命科学", "法律法规", "创业创客"};
-    private String[] culture = {"美食", "影视剧", "阅读", "旅行", "运动", "二次元"};
     private List<NodeCategory> mData = new ArrayList<>();
     private NodeCategoryAdapter mAdapter;
     private RecyclerView mCategoryRecyclerView;
@@ -35,8 +23,6 @@ public class NodeManagerActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        mInflater = LayoutInflater.from(this);
-
         List<Node> lifeNodes = getNodes(R.array.life_nodes_name, R.array.life_nodes_title);
         NodeCategory life = new NodeCategory("生活百科", lifeNodes);
         List<Node> socialNodes = getNodes(R.array.social_nodes_name, R.array.social_nodes_title);
@@ -66,30 +52,6 @@ public class NodeManagerActivity extends BaseActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.node_choose);
-    }
-
-    private void addCategory(int id, String[] nodes) {
-        FlexboxLayout flexbox = (FlexboxLayout) findViewById(id);
-        for (String t : nodes) {
-            final TagView tagView = new TagView(this);
-            final TextView tv = (TextView) mInflater.inflate(R.layout.item_subscribe_node, tagView, false);
-            tv.setText(t);
-            tv.setDuplicateParentStateEnabled(true);
-            tagView.addView(tv);
-            tagView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    tagView.toggle();
-                    if (tagView.isChecked()) {
-                        tv.setTextColor(getResources().getColor(R.color.theme_primary));
-                    } else {
-                        tv.setTextColor(getResources().getColor(R.color.black_50));
-                    }
-                }
-            });
-
-            flexbox.addView(tagView);
-        }
     }
 
     private List<Node> getNodes(int nodeNameArray, int nodeTitleArray) {
