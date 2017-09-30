05c56be65880fd87741a78952a3da5240fec7e99
caoshen <ceeseemail@gmail.com>
2017-10-01 00:27:48 +0800

    add slide up animation

--- library/src/main/java/xyz/dcme/library/anim/AnimUtils.java
+++ library/src/main/java/xyz/dcme/library/anim/AnimUtils.java
@@ -3,6 +3,7 @@ package xyz.dcme.library.anim;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.view.View;
+import android.view.ViewParent;
 
 public class AnimUtils {
 
@@ -17,5 +18,15 @@ public class AnimUtils {
         animatorSet.play(animatorX).with(animatorY);
         animatorSet.setDuration(500);
         animatorSet.start();
+    }
+
+    public static void startSlideUpAnim(View view) {
+        ObjectAnimator animatorAlpha = ObjectAnimator.ofFloat(view, "alpha", 1f, 0f);
+        ViewParent parent = view.getParent();
+        int halfHeight = ((View) parent).getHeight() / 2;
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(view, "translationY", 0f, -halfHeight);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(animatorAlpha).with(animatorY);
+        animatorSet.setDuration(500).start();
     }
 }
--- library/src/main/java/xyz/dcme/library/widget/AppreciateView.java
+++ library/src/main/java/xyz/dcme/library/widget/AppreciateView.java
@@ -1,17 +1,24 @@
 package xyz.dcme.library.widget;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Checkable;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import xyz.dcme.library.R;
+import xyz.dcme.library.anim.AnimUtils;
+
 
 public class AppreciateView extends FrameLayout implements Checkable {
+    private static final int[] CHECK_STATE = {android.R.attr.state_checked};
     private boolean mIsChecked;
     private CheckableImageView mCheckableImageView;
     private TextView mTextView;
@@ -23,15 +30,40 @@ public class AppreciateView extends FrameLayout implements Checkable {
     private OnCheckedChangeListener mOnCheckedChangeListener;
 
     public AppreciateView(@NonNull Context context) {
-        super(context);
+        this(context, null);
     }
 
     public AppreciateView(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public AppreciateView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initViews();
+    }
+
+    private void initViews() {
+        setClickable(true);
+        mCheckableImageView = new CheckableImageView(getContext());
+        mCheckableImageView.setBackgroundResource(R.drawable.bg_appreciate);
+        mCheckableImageView.setDuplicateParentStateEnabled(true);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT);
+        addView(mCheckableImageView, lp);
+
+        mTextView = new TextView(getContext());
+        mTextView.setText("+1");
+        mTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);
+        mTextView.setTextColor(Color.parseColor("#2196F3"));
+        mTextView.setGravity(Gravity.CENTER);
+        mTextView.setVisibility(View.GONE);
+        addView(mTextView, lp);
+    }
+
+    @Override
+    public boolean performClick() {
+        toggle();
+        return super.performClick();
     }
 
     @Override
@@ -53,9 +85,23 @@ public class AppreciateView extends FrameLayout implements Checkable {
     @Override
     public void toggle() {
         setChecked(!mIsChecked);
+        if (mIsChecked) {
+            AnimUtils.startPulseAnim(mCheckableImageView);
+            mTextView.setVisibility(View.VISIBLE);
+            AnimUtils.startSlideUpAnim(mTextView);
+        }
     }
 
     public interface OnCheckedChangeListener {
         void onCheckedChange(View view, boolean isChecked);
+    }
+
+    @Override
+    protected int[] onCreateDrawableState(int extraSpace) {
+        int[] states = super.onCreateDrawableState(extraSpace + 1);
+        if (isChecked()) {
+            mergeDrawableStates(states, CHECK_STATE);
+        }
+        return states;
     }
 }
