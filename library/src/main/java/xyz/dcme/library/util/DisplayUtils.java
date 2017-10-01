d86a54ed3cf04c36594ac0a00edf181ac025b468
caoshen <ceeseemail@gmail.com>
2017-10-01 17:29:17 +0800

    modify appreciate view default attributes

--- library/src/main/java/xyz/dcme/library/util/DisplayUtils.java
+++ library/src/main/java/xyz/dcme/library/util/DisplayUtils.java
@@ -14,6 +14,12 @@ public class DisplayUtils {
         return dp > 0 ? px : dp;
     }
 
+    public static int sp2px(Context context, int sp) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        int px = (int) ((sp * metrics.scaledDensity) + 0.5F);
+        return sp > 0 ? px : sp;
+    }
+
     public static int px2dp(Context context, int px) {
         DisplayMetrics metrics = context.getResources().getDisplayMetrics();
         int dp = (int) ((px / metrics.density) + 0.5F);
--- library/src/main/java/xyz/dcme/library/widget/appreciateview/AppreciateView.java
+++ library/src/main/java/xyz/dcme/library/widget/appreciateview/AppreciateView.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
@@ -14,12 +15,14 @@ import android.widget.TextView;
 
 import xyz.dcme.library.R;
 import xyz.dcme.library.anim.AnimUtils;
+import xyz.dcme.library.util.DisplayUtils;
 
 
 public class AppreciateView extends FrameLayout implements Checkable {
     private static final int[] CHECK_STATE = {android.R.attr.state_checked};
-    public static final float DEFAULT_IMAGE_SIZE = 20F;
-    private static final float DEFAULT_TEXT_SIZE = 10F;
+    public static final String DEFAULT_TEXT = "+1";
+    public static final int DEFAULT_TEXT_SIZE = 4;
+    public static final int DEFAULT_IMAGE_SIZE = 20;
     private float mTextSize;
     private int mTextColor;
     private float mImageSize;
@@ -47,10 +50,10 @@ public class AppreciateView extends FrameLayout implements Checkable {
         super(context, attrs, defStyleAttr);
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AppreciateView);
         mImageBackground = typedArray.getResourceId(R.styleable.AppreciateView_image_background, R.drawable.bg_appreciate);
-        mImageSize = typedArray.getDimension(R.styleable.AppreciateView_image_size, DEFAULT_IMAGE_SIZE);
+        mImageSize = typedArray.getDimension(R.styleable.AppreciateView_image_size, 0);
         mText = typedArray.getString(R.styleable.AppreciateView_text);
         mTextColor = typedArray.getColor(R.styleable.AppreciateView_text_color, getResources().getColor(R.color.checked_color));
-        mTextSize = typedArray.getDimension(R.styleable.AppreciateView_text_size, DEFAULT_TEXT_SIZE);
+        mTextSize = typedArray.getDimension(R.styleable.AppreciateView_text_size, 0);
         typedArray.recycle();
         initViews();
     }
@@ -60,19 +63,29 @@ public class AppreciateView extends FrameLayout implements Checkable {
         mCheckableImageView = new CheckableImageView(getContext());
         mCheckableImageView.setBackgroundResource(mImageBackground);
         mCheckableImageView.setDuplicateParentStateEnabled(true);
-        int width = (int) mImageSize;
-        int height = (int) mImageSize;
-        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(width, height);
+        if (mImageSize == 0) {
+            mImageSize = DisplayUtils.dp2px(getContext(), DEFAULT_IMAGE_SIZE);
+        }
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams((int) mImageSize, (int) mImageSize);
         lp.gravity = Gravity.CENTER;
         addView(mCheckableImageView, lp);
 
         mTextView = new TextView(getContext());
+        if (TextUtils.isEmpty(mText)) {
+            mText = DEFAULT_TEXT;
+        }
+        if (mTextSize == 0) {
+            mTextSize = DisplayUtils.sp2px(getContext(), DEFAULT_TEXT_SIZE);
+        }
         mTextView.setText(mText);
         mTextView.setTextSize(mTextSize);
         mTextView.setTextColor(mTextColor);
         mTextView.setGravity(Gravity.CENTER);
         mTextView.setVisibility(View.GONE);
-        addView(mTextView, lp);
+        FrameLayout.LayoutParams tlp = new FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+        tlp.gravity = Gravity.CENTER;
+        addView(mTextView, tlp);
     }
 
     @Override
