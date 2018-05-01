045c9b2c4185462854e436eecb68996d987ef8f7
caoshen <ceeseemail@gmail.com>
2018-05-01 13:28:25 +0800

    bug-fix:NullPointerException when access network state

--- app/src/main/java/cn/okclouder/ovc/base/NetworkUtils.java
+++ app/src/main/java/cn/okclouder/ovc/base/NetworkUtils.java
@@ -6,6 +6,9 @@ import android.net.NetworkInfo;
 
 public class NetworkUtils {
     public static boolean isConnected(Context context) {
+        if (context == null) {
+            return false;
+        }
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (cm != null) {
             NetworkInfo ni = cm.getActiveNetworkInfo();
